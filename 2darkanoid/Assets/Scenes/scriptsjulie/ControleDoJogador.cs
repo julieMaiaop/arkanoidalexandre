9211065553ea8ee5d79be360e01747b9f248200a
julieMaiaop <167346442+julieMaiaop@users.noreply.github.com>
2024-08-20 13:28:43 -0300

    Update ControleDoJogador.cs

--- 2darkanoid/Assets/Scenes/scriptsjulie/ControleDoJogador.cs
+++ 2darkanoid/Assets/Scenes/scriptsjulie/ControleDoJogador.cs
@@ -7,7 +7,7 @@ public class ControleDoJogador : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
