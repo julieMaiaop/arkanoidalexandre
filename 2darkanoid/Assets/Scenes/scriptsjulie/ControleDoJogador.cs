de537098fd8cb439ca8c41c16d389ae033fa35be
julieMaiaop <167346442+julieMaiaop@users.noreply.github.com>
2024-08-20 14:16:48 -0300

    paddle pronto

--- 2darkanoid/Assets/Scenes/scriptsjulie/ControleDoJogador.cs
+++ 2darkanoid/Assets/Scenes/scriptsjulie/ControleDoJogador.cs
@@ -7,17 +7,42 @@ public class ControleDoJogador : MonoBehaviour
 
     public float velocidadeDoJogador;
 
+    public float xMinimo;
+    public float xMaximo;
     
 
     // Update is called once per frame
     void Update()
     {
+        MovimentarJogador(GetTransform());
         
     }
-    private void MovimentarJogador()
-{
-    
 
-}
+    private Transform GetTransform()
+    {
+        return transform;
+    }
+
+    private void MovimentarJogador(Transform transform)
+
+
+
+        
+        {
+        transform.position = new Vector2(Mathf.Clamp(transform.position.x, xMinimo, xMaximo), transform.position.y);
+        {
+
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector2.left * velocidadeDoJogador * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector2.right * velocidadeDoJogador * Time.deltaTime);
+        }
+          }
 
 }
