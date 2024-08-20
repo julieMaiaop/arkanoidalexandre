de7f5130e514c579e5a82ae0e937a3f914d18058
julieMaiaop <167346442+julieMaiaop@users.noreply.github.com>
2024-08-20 14:38:31 -0300

    Update Bola.cs

--- 2darkanoid/Assets/Scenes/scriptsjulie/Bola.cs
+++ 2darkanoid/Assets/Scenes/scriptsjulie/Bola.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class Bola : MonoBehaviour
 {
+
+
+    public float velocidadeDaBola;
+    public Rigidbody2D oRigidbody2D;
+    public bool jogoIniciou;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(jogoIniciou == false)
+        {
+            //parei na aula 3 minuto 20
+        }
+    }
+
+    private void MovimentarBolinha()
+    {
+        oRigidbody2D.velocity = Vector2.up * velocidadeDaBola;
     }
 }
